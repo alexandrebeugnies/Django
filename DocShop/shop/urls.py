f56b19c0996217018c9696615fd01d7824ba0f83
beugnies alexandre <beugniesalexandre@outlook.com>
2022-08-31 16:25:49 +0200

    resolve Bug

--- DocShop/shop/urls.py
+++ DocShop/shop/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from shop import settings
 
 urlpatterns = [
-    path('', index, name=index),
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     
 ]+static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
