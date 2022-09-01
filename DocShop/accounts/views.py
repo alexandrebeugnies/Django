39b1628334d7371e8d35523384c6d6b320a4ed42
beugnies alexandre <beugniesalexandre@outlook.com>
2022-09-01 11:07:49 +0200

    login,logout functions added

--- DocShop/accounts/views.py
+++ DocShop/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import get_user_model,login,logout
+from django.contrib.auth import get_user_model,login,logout,authenticate
 
 User = get_user_model()
 # Create your views here.
@@ -12,7 +12,19 @@ def signup(request):
         login(request, user)
         return redirect('index')
     return render(request, 'accounts/signup.html')
+def login_user(request):
+    if request.method == 'POST':
+        # Connecter l'utilisateur
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username= username,password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+    return render(request, 'accounts/login.html')
 
 def logout_user(request):
     logout(request)
     return redirect('index')
+
