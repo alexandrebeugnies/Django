d390a208fb9b37078429a2ca3fa831ce1dfef450
beugnies alexandre <beugniesalexandre@outlook.com>
2022-09-01 12:19:04 +0200

    -mCart manage added

--- DocShop/store/admin.py
+++ DocShop/store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from store.models import Order, Product
+from store.models import Cart, Order, Product
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Cart)
