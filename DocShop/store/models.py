7132a2da9ab117f37d1bc69b0f24371512593cc9
beugnies alexandre <beugniesalexandre@outlook.com>
2022-09-01 11:23:01 +0200

    class Order added

--- DocShop/store/models.py
+++ DocShop/store/models.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from django.db import models
 
+from shop.settings import AUTH_USER_MODEL
+
 # Create your models here.
 """
 Produit:
@@ -23,3 +25,28 @@ class Product(models.Model):
         return self.name 
     def get_absolute_url(self):
         return reverse("product",kwargs ={"slug":self.slug})
+
+# Article (Order)
+"""
+- Utilisateur
+- Produit
+- Quantite
+- Commande ou non
+"""
+class Order(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default =1)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})"
+
+# Panier (Cart)
+"""
+- Utilisateur
+- Articles
+- Commande ou non
+- Date de la commande
+"""
+
